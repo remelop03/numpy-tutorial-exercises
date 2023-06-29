c91508f825f28c49c59109514a081f83c88f6340
Ernesto Gonzalez <60261375+UmiKami@users.noreply.github.com>
2023-06-29 19:04:30 +0000

    same as previous exercise | test that checks for reverse operation made more flexible

--- .learn/exercises/009-reverse-vector/test.py
+++ .learn/exercises/009-reverse-vector/test.py
@@ -1,14 +1,24 @@
 import pytest
-import os
+import os,re
 
 @pytest.mark.it("You have to reverse the vector values")
 def test_output():
-    f = open('app.py')
-    content = f.read()
-    assert content.find("[::-1]") > 0
+    path = os.path.dirname(os.path.abspath('app.py'))+'/app.py'
+    with open(path, 'r') as content_file:
+        content = content_file.read()
+        regex = re.compile(r"\s*\:\s*\:\s*\-\s*1\s*")
+        assert bool(regex.search(content)) == True
 
-@pytest.mark.it('The output should be a vector with all the integers from 10 to 1')
+@pytest.mark.it('The output should be a vector with all the integers from 9 to 0 inclusive of both ends')
 def test_print(capsys):
     import app
     captured = capsys.readouterr()
-    assert captured.out == '[9 8 7 6 5 4 3 2 1 0]\n'
+    assert  '[9 8 7 6 5 4 3 2 1 0]\n' in captured.out
+
+@pytest.mark.it("You should not be hard-coding the expected value")
+def test_hard_code():
+    path = os.path.dirname(os.path.abspath('app.py'))+'/app.py'
+    with open(path, 'r') as content_file:
+        content = content_file.read()
+        regex = re.compile(r"\[9 8 7 6 5 4 3 2 1 0\]\\n")
+        assert bool(regex.search(content)) == False
