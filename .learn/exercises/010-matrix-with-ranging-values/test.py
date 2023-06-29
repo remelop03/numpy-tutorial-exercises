3bf66dcd6325946e094fa00c4dca9656513f89e6
Ernesto Gonzalez <60261375+UmiKami@users.noreply.github.com>
2023-06-29 20:10:51 +0000

    made test flexible when checking caputured out | added hardcode check

--- .learn/exercises/010-matrix-with-ranging-values/test.py
+++ .learn/exercises/010-matrix-with-ranging-values/test.py
@@ -1,5 +1,5 @@
 import pytest
-import os
+import os,re 
 
 @pytest.mark.it("You have to use the reshape() method")
 def test_output():
@@ -7,8 +7,16 @@ def test_output():
     content = f.read()
     assert content.find("reshape(") > 0
 
-@pytest.mark.it('The output should be a matrix which values should be from 0 to 9')
+@pytest.mark.it('The output should be a matrix which values should be from 0 to 8')
 def test_print(capsys):
     import app
     captured = capsys.readouterr()
-    assert captured.out == '[[0 1 2]\n [3 4 5]\n [6 7 8]]\n'
+    assert  '[[0 1 2]\n [3 4 5]\n [6 7 8]]\n' in captured.out
+
+@pytest.mark.it("You should not be hard-coding the expected value")
+def test_hard_code():
+    path = os.path.dirname(os.path.abspath('app.py'))+'/app.py'
+    with open(path, 'r') as content_file:
+        content = content_file.read()
+        regex = re.compile(r"\[\[0 1 2\]\\n \[3 4 5\]\\n \[6 7 8\]\]\\n")
+        assert bool(regex.search(content)) == False
