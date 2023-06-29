a9e25bd071deb19e4829d9518d32e0b4a3e96b47
Ernesto Gonzalez <60261375+UmiKami@users.noreply.github.com>
2023-06-29 20:35:35 +0000

    checks for array method

--- .learn/exercises/011-find-indices-of-non-zero-elements/test.py
+++ .learn/exercises/011-find-indices-of-non-zero-elements/test.py
@@ -1,14 +1,28 @@
 import pytest
-import os
+import os, re
 
 @pytest.mark.it("You have to use the nonzero() method")
 def test_output():
     f = open('app.py')
     content = f.read()
-    assert "nonzero(" in content
+    assert "nonzero" in content
+
+@pytest.mark.it("You have to use the array() method")
+def test_array_exists():
+    f = open('app.py')
+    content = f.read()
+    assert "array" in content
 
 @pytest.mark.it('The output should be a tuple of arrays with the indices of non zero values')
 def test_print(capsys):
     import app
     captured = capsys.readouterr()
-    assert captured.out == '(array([0, 1, 4]),)\n'
+    assert   '(array([0, 1, 4]),)\n' in captured.out
+
+@pytest.mark.it("You should not be hard-coding the expected value")
+def test_hard_code():
+    path = os.path.dirname(os.path.abspath('app.py'))+'/app.py'
+    with open(path, 'r') as content_file:
+        content = content_file.read()
+        regex = re.compile(r"\(array\(\[0, 1, 4\]\),\)")
+        assert bool(regex.search(content)) == False
