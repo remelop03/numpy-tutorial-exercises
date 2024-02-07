225af481a6b04e75be43a98990cbfd954ef79760
Jose Mora <109150320+josemoracard@users.noreply.github.com>
2024-02-07 11:36:36 +0100

    Update test.py

--- .learn/exercises/003-numpy-version/test.py
+++ .learn/exercises/003-numpy-version/test.py
@@ -1,7 +1,7 @@
 import os, pytest, re
 import numpy as np
 
-@pytest.mark.it("Import Numpy as np on the app.py file")
+@pytest.mark.it("Import NumPy as np on the app.py file")
 def test_declare_variable():
     path = 'app.py'
     with open(path, 'r') as content_file:
@@ -9,7 +9,7 @@ def test_declare_variable():
         regex = re.compile(r"import\s+numpy\s+as\s+np")
         assert bool(regex.search(content)) == True
 
-@pytest.mark.it("You have to access to the __version__ property.")
+@pytest.mark.it("You have to access to the __version__ property")
 def test_declare_variable():
     path = 'app.py'
     with open(path, 'r') as content_file:
