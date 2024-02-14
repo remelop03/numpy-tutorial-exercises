6f837c473f0a0dda5320121fafe1fe7d26e20de2
Jose Mora <109150320+josemoracard@users.noreply.github.com>
2024-02-14 01:32:44 +0100

    Update solution.hide.py

--- .learn/exercises/017-add-border-to-array/solution.hide.py
+++ .learn/exercises/017-add-border-to-array/solution.hide.py
@@ -1,8 +1,8 @@
 import numpy as np
 
-arr = np.ones((3, 3))
+matrix = np.ones((3, 3))
 
-arr_padded = np.pad(arr, pad_width=1)
+padded_matrix = np.pad(arr, pad_width=1)
 
 
-print(arr_padded)
+print(padded_matrix)
